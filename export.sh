258abe4f83e348cc689f1d36a407715192ee8068
Clemens Anzmann <clemens.anzmann@gmail.com>
2016-11-10 10:50:53 -0500

    move exports into own folder

--- export.sh
+++ export.sh
@@ -1,2 +1,2 @@
-pandoc --from markdown_github cv.md --to html --standalone -c style.css > cv.html
-pandoc cv.md --latex-engine=xelatex -o cv.pdf -V geometry:margin=1in
+pandoc --from markdown_github cv.md --to html --standalone -c style.css > exports/cv.html
+pandoc cv.md --latex-engine=xelatex -o exports/cv.pdf -V geometry:margin=1in
