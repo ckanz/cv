43f63a8ef39b14e93ab4491309657fc7e3ea895e
Clemens Anzmann <clemens.anzmann@gmail.com>
2017-06-10 16:49:08 +0200

    create one new version with descriptions and remove old version

--- export.sh
+++ export.sh
@@ -1,4 +1,2 @@
 pandoc --from markdown_github cv.md --to html --standalone -c style.css > exports/cv.html
-pandoc --from markdown_github cv_2.md --to html --standalone -c style.css > exports/cv_2.html
 pandoc cv.md --latex-engine=xelatex -o exports/cv.pdf -V 'margin-top=1.0in' -V 'margin-bottom=1.0in' -V 'margin-left=0.5in' -V 'margin-right=0.5in' -V 'mainfont:Helvetica' -V 'linkcolor:blue' -V 'pagenumbering=gobble'
-pandoc cv_2.md --latex-engine=xelatex -o exports/cv_2.pdf -V 'margin-top=1.0in' -V 'margin-bottom=1.0in' -V 'margin-left=0.5in' -V 'margin-right=0.5in' -V 'mainfont:Helvetica' -V 'linkcolor:blue' -V 'pagenumbering=gobble'
